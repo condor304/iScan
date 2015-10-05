e5cfcb27be29f483dfb07690be0bd037849c7317
Andrei Sandu <andrei@andreis-macbook-air.local>
2015-10-05 23:41:15 +0300

    Initial Commit

---
+++ iScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iScan
+//
+//  Created by Andrei Sandu on 05/10/15.
+//  Copyright © 2015 Andrei Sandu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iScan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iScan
+//
+//  Created by Andrei Sandu on 05/10/15.
+//  Copyright © 2015 Andrei Sandu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
